2b5ab856c630d5eeb9505e9081d5ea68d4c1dc9b
iambnlvn <iambnlvn@gmail.com>
2024-09-10 20:43:57 -0500

    chore: refactor return_main_menu and finalize french translation support

---
+++ languageData.py
@@ -0,0 +1,98 @@
+welcomeBanner = {
+    'en': 'Lambda Calculus Interpreter',
+    'fr': 'Interpréteur de Calcul Lambda'
+}
+goodByeBanner = {
+    'en': 'Goodbye!',
+    'fr': 'Au revoir!'
+}
+inputMessage = {
+    'en': 'Enter your choice: ',
+    'fr': 'Entrez votre choix: '
+}
+
+termInputMessage = {
+    'en': 'Enter a term: ',
+    'fr': 'Entrez un terme: '
+}
+successMessage = {
+    'en': 'Done!',
+    'fr': 'succès!'
+}
+invalidChoiceMessage = {
+    'en': 'Invalid choice! Enter your choice: ',
+    'fr': 'Choix invalide! Entrez votre choix: '
+}
+
+mainMenuOptions = {
+    'en': {
+        1: 'Run Beta Reduction',
+        2: 'Run Interactive Beta Reduction',
+        3: 'Arithmetic Operations',
+        4: 'Show Numbers',
+        5: 'Boolean Expression',
+        6: 'Exit'
+    },
+    'fr': {
+        1: 'Exécuter la Réduction Bêta',
+        2: 'Exécuter la Réduction Bêta Interactive',
+        3: 'Opérations Arithmétiques',
+        4: 'Afficher les Nombres',
+        5: 'Expression Booléenne',
+        6: 'Quitter'
+    }
+}
+
+arithmetic_operations_options = {
+    'en': {
+        1: 'Addition',
+        2: 'Subtraction',
+        3: 'Multiplication',
+        4: 'Power',
+        5: 'Successor',
+        6: 'Predecessor',
+        7: 'Back',
+    },
+    'fr': {
+        1: 'Addition',
+        2: 'Soustraction',
+        3: 'Multiplication',
+        4: 'Puissance',
+        5: 'Successeur',
+        6: 'Prédécesseur',
+        7: 'Retour',
+    }
+}
+
+choice_number_representation= {
+'en':{
+    1: 'Church Integers',
+    2: 'Relative Integers',
+    3: 'Back'
+},
+'fr':{
+    1: 'Nombres entiers (représentation de Church)',
+    2: 'Nombres relatifs',
+    3: 'Retour'
+}   
+}
+
+boolean_representation= {
+'en':{
+    1: 'NOT',
+    2: 'AND',
+    3: 'OR',
+    4: 'XOR',
+    5: 'IS ZERO',
+    6: 'Back'
+},
+'fr':{
+    1: 'NON',
+    2: 'ET',
+    3: 'OU',
+    4: 'OU EXCLUSIF',
+    5: 'EST NUL',
+    6: 'Retour'
+}
+
+}
--- main.py
+++ main.py
@@ -10,113 +10,11 @@ import pyfiglet
 import keyboard
 import shutil
 import platform
-
+from languageData import *
 if platform.system() == 'Windows':
     clear = lambda: os.system('cls')
 else:
     clear = lambda: os.system('clear')
-
-
-welcomeBanner = {
-    'en': 'Lambda Calculus Interpreter',
-    'fr': 'Interpréteur de Calcul Lambda'
-}
-goodByeBanner = {
-    'en': 'Goodbye!',
-    'fr': 'Au revoir!'
-}
-inputMessage = {
-    'en': 'Enter your choice: ',
-    'fr': 'Entrez votre choix: '
-}
-
-termInputMessage = {
-    'en': 'Enter a term: ',
-    'fr': 'Entrez un terme: '
-}
-successMessage = {
-    'en': 'Done!',
-    'fr': 'succès!'
-}
-invalidChoiceMessage = {
-    'en': 'Invalid choice! Enter your choice: ',
-    'fr': 'Choix invalide! Entrez votre choix: '
-}
-
-mainMenuOptions = {
-    'en': {
-        1: 'Run Beta Reduction',
-        2: 'Run Interactive Beta Reduction',
-        3: 'Arithmetic Operations',
-        4: 'Show Numbers',
-        5: 'Boolean Expression',
-        6: 'Exit'
-    },
-    'fr': {
-        1: 'Exécuter la Réduction Bêta',
-        2: 'Exécuter la Réduction Bêta Interactive',
-        3: 'Opérations Arithmétiques',
-        4: 'Afficher les Nombres',
-        5: 'Expression Booléenne',
-        6: 'Quitter'
-    }
-}
-
-arithmetic_operations_options = {
-    'en': {
-        1: 'Addition',
-        2: 'Subtraction',
-        3: 'Multiplication',
-        4: 'Power',
-        5: 'Successor',
-        6: 'Predecessor',
-        7: 'Back',
-    },
-    'fr': {
-        1: 'Addition',
-        2: 'Soustraction',
-        3: 'Multiplication',
-        4: 'Puissance',
-        5: 'Successeur',
-        6: 'Prédécesseur',
-        7: 'Retour',
-    }
-}
-
-choice_number_representation= {
-'en':{
-    1: 'Church Integers',
-    2: 'Relative Integers',
-    3: 'Back'
-},
-'fr':{
-    1: 'Nombres entiers (représentation de Church)',
-    2: 'Nombres relatifs',
-    3: 'Retour'
-}   
-}
-
-
-
-boolean_representation= {
-'en':{
-    1: 'NOT',
-    2: 'AND',
-    3: 'OR',
-    4: 'XOR',
-    5: 'IS ZERO',
-    6: 'Back'
-},
-'fr':{
-    1: 'NON',
-    2: 'ET',
-    3: 'OU',
-    4: 'OU EXCLUSIF',
-    5: 'EST NUL',
-    6: 'Retour'
-}
-
-}
 
 
 def delete_images(folder):
@@ -134,59 +32,68 @@ def moveImages(src, dest):
             os.remove(dest+'/'+filename)
          shutil.move(src+"/"+filename, dest)
 
-def return_main_menu():
-    image_maker.colors = ['black','blue','green','orange','pink','purple','red','yellow']
-    not_pressed = True
-    print('Press ENTER to return to the main menu')
-    while not_pressed:  # making a loop
-        try:  # used try so that if user pressed other than the given key error will not be shown
-            if keyboard.is_pressed('ENTER'):  # if key 'ENTER' is pressed 
-                not_pressed = False  # finishing the loop
-                clear()
-                time.sleep(1)
-                logic.image_counter = 0
-                image_maker.variables_colors_couple = {}
+def wait_for_enter(language):
+    print('Press ENTER to return to the main menu' if language == 'en' else 'Appuyez sur ENTRER pour revenir au menu principal')
+    while True:
+        try:
+            if keyboard.is_pressed('ENTER'):
                 break
         except:
-            break  # if user pressed a key other than the given key the loop will break
+            break
+
+def reset_state():
+    clear()
+    time.sleep(1)
+    logic.image_counter = 0
+    image_maker.variables_colors_couple = {}
+
+def return_main_menu(language='en'):
+    image_maker.colors = ['black', 'blue', 'green', 'orange', 'pink', 'purple', 'red', 'yellow']
+    wait_for_enter(language)
+    reset_state()
     run_main_menu()
 
-def run_beta_reduction_totale(terme,path='beta_reduction_totale'):
+def run_beta_reduction_totale(terme,language='en'):
+    path='beta_reduction_totale'
+    directory = "./saved/" if language == 'en' else "./sauvegarde/"
     terme = parsing.parseTerm(terme)
     if logic.recognize_term(terme):
-        print(" terme infini detecté")
+        print("terme infini detecté" if language == 'fr' else 'infinite term detected')
         return None
-    os.makedirs(path, exist_ok=True)# cree le rep si il existe pas, le vide si non
+    os.makedirs(path, exist_ok=True)
     if len(os.listdir(path)) > 0:
         logic.image_counter = 0
     k=logic.beta_reduction_totale(terme,path)
-    print(" le terme obtenu est: "+logic.to_string(k))
-    save_image_choice = input('Do you want to save the images? (Y/n): ')
-    while save_image_choice not in ['y','n','']:
-            save_image_choice = input('Invalid choice. Do you want to save the images? (Y/n): ')
-    if save_image_choice == 'y' or save_image_choice=='':
-        os.makedirs("./sauvegarde/"+path, exist_ok=True)
-        moveImages(path,'./sauvegarde/'+path)
+    msg = "le terme obtenu est: " if language == 'fr' else 'the obtained term is: '
+    print(msg +logic.to_string(k))
+    save_image_choice = input('Do you want to save the images? (Y/n): ' if language == 'en' else 'Voulez-vous enregistrer les images? (O/n): ').lower()
+    while save_image_choice not in ['y','n','','o']:
+        save_image_choice = input('Invalid choice. Do you want to save the images? (Y/n): ' if language == 'en' else 'Choix invalide. Voulez-vous enregistrer les images? (O/n): ').lower()
+    if save_image_choice == 'y' or save_image_choice=='' or save_image_choice=='o': 
+        os.makedirs(directory+path, exist_ok=True)
+        moveImages(path,directory+path)
         os.rmdir(path)
     elif save_image_choice=='n':
         delete_images(path)
         os.rmdir(path)
 
-def run_beta_reduction_interactive_totale(terme,path='beta_reduction_interactive_totale'):
+def run_beta_reduction_interactive_totale(terme,language='en'):
+    path='beta_reduction_interactive_totale'
+    directory = "./saved/" if language == 'en' else "./sauvegarde/"
     terme = parsing.parseTerm(terme)
     if logic.recognize_term(terme):
-        print(" terme infini detecté")
+        print(" terme infini detecté" if language == 'fr' else 'infinite term detected')
         return None
     os.makedirs(path, exist_ok=True)
     if len(os.listdir(path)) > 0:
         logic.image_counter = 0
     logic.beta_reduction_interactive_totale(terme,path)
-    save_image_choice = input('Do you want to save the images? (Y/n): ')
-    while save_image_choice not in ['y','n','']:
-            save_image_choice = input('Invalid choice. Do you want to save the images of the reduction? (Y/n): ')
-    if save_image_choice == 'y'or save_image_choice=='':
-        os.makedirs("./sauvegarde/"+path, exist_ok=True)
-        moveImages(path,'./sauvegarde/'+path)
+    save_image_choice = input('Do you want to save the images? (Y/n): ' if language == 'en' else 'Voulez-vous enregistrer les images? (O/n): ').lower()
+    while save_image_choice not in ['y','n','','o']:
+        save_image_choice = input('Invalid choice. Do you want to save the images of the reduction? (Y/n): ' if language == 'en' else 'Choix invalide. Voulez-vous enregistrer les images de la réduction? (O/n): ').lower()
+    if save_image_choice == 'y'or save_image_choice=='' or save_image_choice=='o':
+        os.makedirs(directory+path, exist_ok=True)
+        moveImages(path,directory+path)
         os.rmdir(path)
     elif save_image_choice == 'n':
         delete_images(path)
@@ -195,12 +102,12 @@ def run_beta_reduction_interactive_totale(terme,path='beta_reduction_interactive
 def run_show_numbers(path,language='en'):
     os.makedirs(path, exist_ok=True)
     print_menu(choice_number_representation,language)
+    msg = 'Voici le terme:' if language == 'fr' else 'Here is the term:'
+    directory = "./saved/" if language == 'en' else "./sauvegarde/"
     choice = int(input(inputMessage[language]))
     while choice not in mainMenuOptions[language]:
         clear()
         print_menu(choice_number_representation,language)
-        msg = 'Voici le terme:' if language == 'fr' else 'Here is the term:'
-        directory = "./saved/" if language == 'en' else "./sauvegarde/"
         choice = int(input('Invalid choice. Enter your choice: ' if language == 'en' else 'Choix invalide. Entrez votre choix: '))
     if choice ==1:
         clear()
@@ -278,7 +185,7 @@ def run_boolean_expression(path,language='en'):
         logic.captureImage(t,path,'AND',False)
         save_image_choice = input('Do you want to save the images? (Y/n): ' if language == 'en' else 'Voulez-vous enregistrer les images? (O/n): ').lower()
         while save_image_choice not in ['y','n','o','']:
-                save_image_choice = input('Invalid choice. Do you want to save the images of the reduction? (Y/n): ' if language == 'en' else 'Choix invalide. Voulez-vous enregistrer les images de la réduction? (O/n): ').lower()
+            save_image_choice = input('Invalid choice. Do you want to save the images of the reduction? (Y/n): ' if language == 'en' else 'Choix invalide. Voulez-vous enregistrer les images de la réduction? (O/n): ').lower()
         if save_image_choice == 'y' or save_image_choice=='' or save_image_choice=='o':
             directory = "./saved/" if language == 'en' else "./sauvegarde/"
             os.makedirs(directory+path, exist_ok=True)
@@ -297,7 +204,7 @@ def run_boolean_expression(path,language='en'):
         while save_image_choice not in ['y','n','o','']:
                 save_image_choice = input('Invalid choice. Do you want to save the images of the reduction? (Y/n): ' if language == 'en' else 'Choix invalide. Voulez-vous enregistrer les images de la réduction? (O/n): ').lower()
         if save_image_choice == 'y' or save_image_choice=='' or save_image_choice=='o':
-            directory = "./saved/" if language == 'en' else "./sauvegarde/"
+
             os.makedirs(directory+path, exist_ok=True)
             moveImages(path,directory+path)
             os.rmdir(path)
@@ -371,7 +278,7 @@ def run_main_menu():
     if choice == 1:
         clear()
         terme1 = input(termInputMessage[language])
-        k=run_beta_reduction_totale(terme1)
+        k=run_beta_reduction_totale(terme1,language)
         if k!=None:
           clear()
           print(successMessage[language])
@@ -381,7 +288,7 @@ def run_main_menu():
     elif choice == 2:
         clear()
         terme2 = input(termInputMessage[language])
-        v= run_beta_reduction_interactive_totale(terme2)
+        v= run_beta_reduction_interactive_totale(terme2,language)
         if v!=None:
           clear()
           print(successMessage[language])
@@ -407,16 +314,16 @@ def run_arithmetic_operations_menu(language='en'):
     path='arithmetic expressions'
     os.makedirs(path, exist_ok=True)
     print_menu(arithmetic_operations_options,language)
+    msg = 'Voici le terme:' if language == 'fr' else 'Here is the term:'
+    directory = "./saved/" if language == 'en' else "./sauvegarde/"
     choice = int(input('Enter your choice: ' if language == 'en' else 'Entrez votre choix: '))
     while choice not in arithmetic_operations_options[language]:
         clear()
         print_menu(arithmetic_operations_options)
-        msg = 'Voici le terme:' if language == 'fr' else 'Here is the term:'
-        directory = "./saved/" if language == 'en' else "./sauvegarde/"
         choice = int(input('Invalid choice. Enter your choice: ' if language == 'en' else 'Choix invalide. Entrez votre choix: '))
     if choice == 1:
         clear()
-        print(msg+ logic.to_string(app_functions.ADD))
+        print(msg + logic.to_string(app_functions.ADD))
         logic.captureImage(app_functions.ADD,path,'ADD', False)
         choix=(input("Do you want to try an example? (Y/n) : " if language == 'en' else 'Voulez-vous essayer un exemple? (O/n) : ')).lower()
         while choix not in ['y','n','o','']:
@@ -443,7 +350,7 @@ def run_arithmetic_operations_menu(language='en'):
         elif save_image_choice == 'n':
             delete_images(path)
             os.rmdir(path)
-        return_main_menu()
+        return_main_menu(language)
     elif choice == 2:
         clear()
         print(msg+ logic.to_string(app_functions.SUB))
@@ -472,7 +379,7 @@ def run_arithmetic_operations_menu(language='en'):
         elif save_image_choice == 'n':
             delete_images(path)
             os.rmdir(path)
-        return_main_menu()
+        return_main_menu(language)
     elif choice == 3:
         clear()
         print(msg+ logic.to_string(app_functions.MUL))
@@ -501,7 +408,7 @@ def run_arithmetic_operations_menu(language='en'):
         elif save_image_choice == 'n':
             delete_images(path)
             os.rmdir(path)
-        return_main_menu()
+        return_main_menu(language)
     elif choice==4:
         clear()
         print(msg+ logic.to_string(app_functions.POW))
@@ -530,7 +437,7 @@ def run_arithmetic_operations_menu(language='en'):
         elif save_image_choice == 'n':
             delete_images(path)
             os.rmdir(path)
-        return_main_menu()
+        return_main_menu(language)
     elif choice==5:
         clear()
         print(msg+ logic.to_string(app_functions.SUCCS))
@@ -557,7 +464,7 @@ def run_arithmetic_operations_menu(language='en'):
         elif save_image_choice == 'n':
             delete_images(path)
             os.rmdir(path)
-        return_main_menu()
+        return_main_menu(language)
     elif choice==6:
         clear()
         print(msg+ logic.to_string(app_functions.PRED))
@@ -584,7 +491,7 @@ def run_arithmetic_operations_menu(language='en'):
         elif save_image_choice == 'n':
             delete_images(path)
             os.rmdir(path)
-        return_main_menu()
+        return_main_menu(language)
     elif choice == 7:
         clear()
         run_main_menu()
